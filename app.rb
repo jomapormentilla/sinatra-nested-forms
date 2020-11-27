854dc6b485e892a0e9d829088d2bb0ec1d083fa3
jomapormentilla <jomapormentilla@gmail.com>
2020-11-27 16:12:40 -0500

    commit tree 2a288c584ec573da7b1cb720f7e7c6b7e281f87f

--- app.rb
+++ app.rb
@@ -1,9 +1,48 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      "Welcome to the Nested Forms Lab!"
+    end
 
+    get '/new' do
+      erb :'/pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship)
+      end
+
+      @ships = Ship.all
+
+      erb :'/pirates/show'
+    end
   end
 end
+
+# params =  {
+#     "pirate"=>  {
+#         "name"=>"Joma", 
+#         "weight"=>"150", 
+#         "booty"=>"60", 
+#         "ships"=> [
+#             {
+#                 "name"=>"ShipOne", 
+#                 "type"=>"Big", 
+#                 "booty"=>"Long"
+#             }, 
+#             {
+#                 "name"=>"ShipTo", 
+#                 "type"=>"Medium", 
+#                 "booty"=>"Short"
+#             }
+#         ]
+#     }
+# }
